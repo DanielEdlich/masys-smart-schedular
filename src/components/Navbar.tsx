50f84ced2e610914cb69ebf5409b94e48f15cc9a
RemodifyBerlin <remodifyBerlin@gmail.com>
2024-12-07 13:42:20 +0100

    Fix typo in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ export const Navbar = () => {
           </Link>
           <nav className="hidden md:flex gap-6">
             <Link className="text-sm font-medium text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white" href="/">Start</Link>
-            <Link className="text-sm font-medium text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white" href="/lehrer-verwaltung">Lehrer Verwaltung</Link>
+            <Link className="text-sm font-medium text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white" href="/lehrer-verwaltung">Lehrerverwaltung</Link>
             <Link className="text-sm font-medium text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white" href="/#green-code">Green Code</Link>
           </nav>
         </div>
