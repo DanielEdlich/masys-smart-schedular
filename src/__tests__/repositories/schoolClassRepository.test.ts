82c18a05bd42226a96dce3e776b87c8baa8d4139
calipee <54059879+calipee@users.noreply.github.com>
2024-12-08 16:17:29 +0100

    Add global setup for Jest with SQLite database integration

--- src/__tests__/repositories/schoolClassRepository.test.ts
+++ src/__tests__/repositories/schoolClassRepository.test.ts
@@ -6,27 +6,16 @@ import { schoolClass } from '@/db/schema';
 import { SchoolClass, NewSchoolClass } from '@/db/types';
 import { SchoolClassRepository } from '@/repositories/schoolClassRepository';
 
-import Database from 'better-sqlite3';
-import { BetterSQLite3Database, drizzle } from 'drizzle-orm/better-sqlite3';
-import { SQLiteTableWithColumns, SQLiteColumn } from 'drizzle-orm/sqlite-core';
-
-let db: BetterSQLite3Database<{ schoolClass: SQLiteTableWithColumns<{ name: "school_class"; schema: undefined; columns: { id: SQLiteColumn<{ name: "id"; tableName: "school_class"; dataType: "number"; columnType: "SQLiteInteger"; data: number; driverParam: number; notNull: true; hasDefault: true; isPrimaryKey: true; isAutoincrement: false; hasRuntimeDefault: false; enumValues: undefined; baseColumn: never; identity: undefined; generated: undefined; }, object>; name: SQLiteColumn<{ name: "name"; tableName: "school_class"; dataType: "string"; columnType: "SQLiteText"; data: string; driverParam: string; notNull: true; hasDefault: false; isPrimaryKey: false; isAutoincrement: false; hasRuntimeDefault: false; enumValues: [string, ...string[]]; baseColumn: never; identity: undefined; generated: undefined; }, object>; year: SQLiteColumn<{ name: "year"; tableName: "school_class"; dataType: "string"; columnType: "SQLiteText"; data: string; driverParam: string; notNull: false; hasDefault: false; isPrimaryKey: false; isAutoincrement: false; hasRuntimeDefault: false; enumValues: [string, ...string[]]; baseColumn: never; identity: undefined; generated: undefined; }, object>; }; dialect: "sqlite"; }>; }>;
 
 describe('SchoolClassRepository', () => {
   let repo: SchoolClassRepository;
 
   beforeAll(async () => {
-    // Create an in-memory SQLite DB
-    const sqlite = new Database('test.db');
 
-    // Create a fresh drizzle instance connected to the in-memory DB
-    db = drizzle(sqlite)
-
-    // Instantiate the repository with this in-memory db instance
-    repo = new SchoolClassRepository(db);
+    repo = new SchoolClassRepository((global as any).db);
   });
 
-  afterAll(async () => await db.delete(schoolClass))
+  afterAll(async () => await (global as any).db.delete(schoolClass))
 
   describe('create', () => {
     it('should create a new school class and return it', async () => {
