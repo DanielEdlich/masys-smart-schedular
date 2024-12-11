1eec314641c5dc011531bf670d2c80f16188bda3
RemodifyBerlin <remodifyBerlin@gmail.com>
2024-12-11 08:40:51 +0100

    add Toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Toast } from "@radix-ui/react-toast";
+import { ToastProvider } from "@/components/ui/toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         {children}
       </body>
-      <Toast />
+      <ToastProvider />
     </html>
   );
 }
