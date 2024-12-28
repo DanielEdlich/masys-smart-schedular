1da8b6b9f90287f1e24a365e745c157af27aa326
calipee <54059879+calipee@users.noreply.github.com>
2024-12-28 22:09:37 +0100

    fix: update global setup to use libsql client for database connection

--- jest.global-setup.ts
+++ jest.global-setup.ts
@@ -2,16 +2,21 @@
  * @jest-environment node
  */
 import Database from 'better-sqlite3';
-import { drizzle } from 'drizzle-orm/better-sqlite3';
+// import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
+
 
 module.exports = async () => {
   console.log('Setting up global SQLite DB');
 
   // Erstelle eine Datenbank-Datei (kann auch in-memory ':memory:' sein)
-  const sqlite = new Database('test.db'); // oder ':memory:' für in-memory
-
+  // const sqlite = new Database('test.db'); // oder ':memory:' für in-memory
+const client = createClient({
+  url: 'file:test.db',
+});
   // Drizzle-Datenbank-Instanz erstellen
-  const db = drizzle(sqlite);
+  const db = drizzle(client);
 
   // Repository bereitstellen (global zugänglich)
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
